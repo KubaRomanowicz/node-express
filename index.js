3c8a180bb6b622292f48826838dd6edc63f392bf
KubaRomanowicz <kubaromanowicz03@wp.pl>
2021-12-07 15:32:18 +0100

    ggadasd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-setTimeout(function() {
-    console.log('First');
-}, 3000);
-console.log('Second');
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+});
 
 
 
