f5eccd9d7a5cd773200b0a313c1eae073f2f8468
KubaRomanowicz <kubaromanowicz03@wp.pl>
2021-12-07 15:33:02 +0100

    alsdiuasb

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-// wiki.js - Wiki route module
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
 
-const express = require('express');
-const router = express.Router();
-
-// Home page route
-router.get('/', function(req, res) {
-    res.send('Wiki home page');
-});
-
-// About page route
-router.get('/about', function(req, res) {
-    res.send('About this wiki');
-});
-
-module.exports = router;
 
 
 
