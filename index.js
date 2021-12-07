79d1b0abd8db732c6d2fac2d8b79a5ab5ce3acfd
KubaRomanowicz <kubaromanowicz03@wp.pl>
2021-12-07 15:49:12 +0100

    daskfnadas

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const express = require('express');
-const logger = require('morgan');
 const app = express();
-app.use(logger('dev'));
+const port = 3000;
 
-// An example middleware function
-let a_middleware_function = function(req, res, next) {
-    // ... perform some operations
-    next(); // Call next() so Express will call the next middleware function in the chain.
-}
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+});
 
-// Function added with use() for all routes and verbs
-app.use(a_middleware_function);
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`)
+});
 
-// Function added with use() for a specific route
-app.use('/someroute', a_middleware_function);
-
-// A middleware function added for a specific HTTP verb and route
-app.get('/', a_middleware_function);
-
-app.listen(3000);
 
 
 
